6a7456e4f62cdf4071158c9c37dc5c8a455c5f8c
Matthew Gall <me@matthewgall.dev>
2023-11-24 09:12:21 +0000

    Added new race logic to try and achieve faster responses by choosing three providers, and racing them

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,20 @@ Array.prototype.sample = function(){
 	return this[Math.floor(Math.random()*this.length)];
 }
 
+Array.prototype.sampleN = function(n: any) {
+	var result = new Array(n),
+		len = this.length,
+		taken = new Array(len);
+	if (n > len)
+		throw new RangeError("getRandom: more elements taken than available");
+	while (n--) {
+		var x = Math.floor(Math.random() * len);
+		result[n] = this[x in taken ? taken[x] : x];
+		taken[x] = --len in taken ? taken[len] : len;
+	}
+	return result;
+}
+
 router.get('/dns-query', async (request) => {
 	return new Response('Not Implemented', { status: 400 })
 })
@@ -23,25 +37,41 @@ router.post('/dns-query', async (request) => {
 	let q: any = await request.arrayBuffer();
 	q = Buffer.from(q);
 
-	// Next, we prepare to send it on, first pick a resolver (set to our default set)
-	let resolver: any = Config['default'].sample()
-	if (Config[url.hostname]) {
-		// Check now for a resolvers set for the hostname the request came in on
-		resolver = Config[url.hostname].sample()
-	}
-	
 	// Now, prepare the payload
 	q = q.toString('base64').replace(/=+/, '');
 
-	// And send it off
-	let answer: any = await fetch(`${Resolvers[resolver]}?dns=${q}`, {
-		headers: {
-			'Content-Type': 'application/dns-message'
+	// Next, we prepare to send it on, first pick a resolver (by default, we use the default)
+	let resolver: any = Config['default']
+	if (Config[url.hostname]) {
+		// Check now for a resolvers set for the hostname the request came in on
+		resolver = Config[url.hostname]
+	}
+
+	let promises = [];
+	if (resolver.length > 3) {
+		// We are going to race three for a response
+		for (let r of resolver.sampleN(3)) {
+			promises.push(fetch(`${Resolvers[r]}?dns=${q}`, {
+				headers: {
+					'Content-Type': 'application/dns-message'
+				}
+			}))
 		}
-	})
+	}
+	else {
+		// Otherwise, pick one
+		promises.push(fetch(`${Resolvers[resolver.sample()]}?dns=${q}`, {
+			headers: {
+				'Content-Type': 'application/dns-message'
+			}
+		}))
+	}
+	
+	// And send it off
+	let answers: any = await Promise.any(promises);
 
 	// Once we have an answer, we return that
-	return answer
+	return answers
 })
 
 router.get('/dns-providers', async (request) => {
